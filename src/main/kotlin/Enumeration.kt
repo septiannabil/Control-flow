7d22595a7669436443575995cc25e41c72594e21
septiannabil <68981580+septiannabil@users.noreply.github.com>
2020-09-03 04:05:25 +0700

    Add files via upload

---
+++ src/main/kotlin/Enumeration.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val getPlayerLevel = (playerID.Setyo.value)
+    print("Player Level is $getPlayerLevel")
+}
+
+///Enumeration Class
+enum class playerID( val value: Int){
+    Setyo(15),
+    NGR(16),
+    Nugroho(74)
+}
